43da887781a6ece05efc923c5096abc3614f4e2f
Augustin <augustin.levasseur@etu.unilasalle.fr>
2023-10-14 11:56:30 +0200

    fix (horraire): auto load horraire with phone AND computer

--- views/script/submitHorraire.js
+++ views/script/submitHorraire.js
@@ -1,5 +1,11 @@
-console.log("chargéHorraire")
+console.log("submitHorraireLoaded")
 var form = document.getElementById('formulaireHoraire')
+console.log(form)
+
+if (window.innerWidth < 737)
+{
+  var form = document.getElementById('formulaireHoraire-phone')
+}
 
 form.addEventListener('input', function(event){
   form.submit()
